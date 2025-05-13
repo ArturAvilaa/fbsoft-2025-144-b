98f21124336d6f37fae17087a96de0c38ea9bd7e
Deivid Matos <deividdasilvamatos70@gmail.com>
2025-05-13 20:41:00 -0300

    ajuste de arquivo rest

--- projfabsoft/src/main/java/br/univille/projfabsoft/controller/EnderecoController.java
+++ projfabsoft/src/main/java/br/univille/projfabsoft/controller/EnderecoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/enderecos")
+@RequestMapping("/api/v1/enderecos")
 public class EnderecoController {
 
     @Autowired
