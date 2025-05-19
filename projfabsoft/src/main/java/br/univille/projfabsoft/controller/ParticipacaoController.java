5e7ff3240e44bc3fb2e05217bcfaa3b7c5eaae42
Deivid Matos <deividdasilvamatos70@gmail.com>
2025-05-19 20:03:23 -0300

    finalizando endpoints

--- projfabsoft/src/main/java/br/univille/projfabsoft/controller/ParticipacaoController.java
+++ projfabsoft/src/main/java/br/univille/projfabsoft/controller/ParticipacaoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/participacoes")
+@RequestMapping("/api/v1/participacoes")
 public class ParticipacaoController {
 
     @Autowired
